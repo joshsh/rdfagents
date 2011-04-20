0db06ce0fd0c4de996018cde508b9c53530ce37d
Joshua Shinavier <josh@fortytwo.net>
2011-04-20 13:37:59 +0800

    Added RDFAgents spec draft (in progress).

---
+++ core/src/main/java/net/fortytwo/rdfagents/RDFAgents.java
@@ -0,0 +1,11 @@
+package net.fortytwo.rdfagents;
+
+/**
+ * A collection of global constants for the RDFAgents messaging API.
+ */
+public interface RDFAgents {
+    // Agent profile properties
+    public static final String
+            QUERY_ANSWERING_SUPPORTED = "behavior.query.answer.supported",
+            QUERY_ASKING_SUPPORTED = "behavior.query.ask.supported";
+}
---
+++ jade/src/main/java/net/fortytwo/rdfagents/jade/ReceivingBehavior.java
@@ -0,0 +1,29 @@
+package net.fortytwo.rdfagents.jade;
+
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import net.fortytwo.rdfagents.jade.MessageHandler;
+
+/**
+ * User: josh
+ * Date: 3/11/11
+ * Time: 5:20 PM
+ */
+public class ReceivingBehavior extends CyclicBehaviour {
+    private final MessageHandler handler;
+
+    public ReceivingBehavior(MessageHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void action() {
+        ACLMessage m = myAgent.receive();
+        if (null != m) {
+            System.out.println("## received message: " + m);
+            handler.handle(m);
+        } else {
+            block();
+        }
+    }
+}
